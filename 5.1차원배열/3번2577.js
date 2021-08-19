04a488633be2033f6420a64e124f11c5a052b9eb
yoru.lee <yoru.lee7@gmail.com>
2021-08-20 01:32:35 +0900

    2577번 다른스타일로 풀이

--- 5.1차원배열/3번2577.js
+++ 5.1차원배열/3번2577.js
@@ -1,28 +1,28 @@
-// 1번 케이스
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-let three_string = String(input[0] * input[1] * input[2]);
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+const numbers = input.map((item) => +item);
 
-    for(let i = 0; i<10; i++) {
-        
-      let count = 0;
-        
-        for(let j=0; j<three_string.length; j++) {
-            
-                if(i === Number(three_string[j])) {
-                     count++;
-            }
-        
-        } 
-        console.log(count);
-    }
-    
-        // 2번 케이스
 
-        let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+solution(numbers);
 
-let num = String(input[0] * input[1] * input[2]);
 
-for (let i = 0; i <= 9; i++) { 
-   console.log(num.split(i + '').length - 1);    
+function solution(numbers) {
+
+  const number = numbers[0] * numbers[1] * numbers[2]
+  const strNum = String(number)
+  const answer = Array(10).fill(0)
+
+  for (let i of strNum) {
+
+    let A = +strNum[i];
+    answer[A]++;
+
+  }
+
+  for (let j of answer) {
+    console.log(answer[j]);
+  }
+
+
 }
-  
